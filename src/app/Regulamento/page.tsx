392985409e4262ddb73108750535a1be4fdc3008
Daniyutak <dtakara1101@gmail.com>
2025-09-08 19:00:36 -0300

    create: regulamento page

--- src/app/Regulamento/page.tsx
+++ src/app/Regulamento/page.tsx
@@ -1,1 +1,14 @@
 import { useState } from 'react';
+import Footer from "../../components/HeaderFooter/Footer";
+import Header from "../../components/HeaderFooter/Header";
+
+const Regulamento = () =>{
+    return(
+        <div className="page">
+            <Header />
+
+            <Footer />
+        </div>
+
+
+    );}
