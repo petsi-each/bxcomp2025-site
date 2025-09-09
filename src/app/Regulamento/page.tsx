f7b48b369b636db60388c352abd0859bc78d49c3
Daniyutak <dtakara1101@gmail.com>
2025-09-09 19:34:34 -0300

    fix: regulamento page

--- src/app/Regulamento/page.tsx
+++ src/app/Regulamento/page.tsx
@@ -1,14 +1,45 @@
 import { useState } from 'react';
 import Footer from "../../components/HeaderFooter/Footer";
 import Header from "../../components/HeaderFooter/Header";
+import regulamento from "../../data/regulamento.json"
 
-const Regulamento = () =>{
-    return(
-        <div className="page">
-            <Header />
+export default function Regulamento(){
 
-            <Footer />
-        </div>
+    const datas = regulamento.modificacoes.map(mod => {
+        const [day, month, year] = mod.data.split('/').map(Number);
+        return new Date(year, month - 1, day);
+    });
 
+    const latestDate = new Date(Math.max(...datas.map(data => data.getTime())));
 
-    );}
+    return (
+        <>
+        <Header />
+        <main className='mt-16 text-center'>
+            
+            <h1 className='text-ouro text-6xl py-12'>Regulamento 2025</h1>
+
+            <p className=''><b>Última atualização em:</b> {latestDate.toLocaleDateString('pt-BR')}</p>
+
+            <div className="w-full flex items-center justify-center px-4">
+            <iframe
+                src={regulamento.arquivo}
+                title="Regulamento"
+                className="py-4 w-full max-w-[50rem] h-[36rem] lg:h-[50rem]"
+            >
+            </iframe>
+            </div>
+            <ul className={`flex flex-col items-center`}>
+                {
+                    regulamento.modificacoes.map((modificacao, idx) => {
+                        return <li key={idx}><b>{modificacao.tipo}</b> em: {modificacao.data}</li>
+                    })
+                }
+
+            </ul>
+
+        </main>
+        <Footer />
+        </>
+    );
+}
