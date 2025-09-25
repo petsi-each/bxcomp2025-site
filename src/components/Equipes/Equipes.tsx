b9aab24b767de8d0f275df9805bb81855ec7f60b
ARTHUR HERNANDES <hardarthur5@gmail.com>
2025-09-24 22:35:34 -0300

    equipes ate onde deu

--- src/components/Equipes/Equipes.tsx
+++ src/components/Equipes/Equipes.tsx
@@ -34,52 +34,50 @@ const Equipes: React.FC<EquipesProps> = ({equipes}) => {
     }
     
     return (
-    <section className="pb-8 pt-16 sm:px-24 bg-creme grid grid-cols-1 lg:grid-cols-2 justify-center padding">
-    
-        {/* parte que mostra os botões de todas as equipes */}
-        <section className="order-2 lg:order-1 my-4 md:my-2 py-2 md:px-8 lg:px-0 flex flex-wrap items-center justify-center">
-            <section className="gap-8 sm:grid-flow-col flex sm:h-fit flex-wrap items-center justify-center">
-                { equipes.map((equipe, idx) => {
-                    return <BotaoEquipe 
-                            key={idx} 
-                            equipe={equipe} 
-                            selecionado={idSelecionado==idx} 
-                            onClick={()=> {setIdSelecionado(idx); scrolltodisplayer();}} />  
-                })
-                }
-
-            </section>
-        </section>
-
-        <section className="order-1 lg:order-2 px-4 flex flex-col items-center justify-center">
-            {/* titulo do componente e descrição da seção  */}       
-            <h1 id="display" className="text-verdeBX text-6xl"> Equipes </h1>
-            <p className= {`${poppins.className} p-4 text-sm text-center text-pretoBX`}> Conheça os competidores deste ano! </p>
-        
-            {/* circulo, imagem da equipe selecionada e folhinha que indica pontos da equipe modo desktop */}
-            <section className="flex justify-center relative mx-4 my-2 h-60 w-60 sm:h-auto sm:w-auto">
-    
-                <div className="z-10 w-56 h-56 flex justify-center items-center relative border-laranjaBX border-8 p-2 rounded-full bg-white sm:w-72 sm:h-72 drop-shadow-lg"> 
-                <Image className="z-0 w-fit h-fit" alt="" width={130} height={130} src={equipes[idSelecionado].iconPath} />
-                </div>
-                <div className="z-30 text-center -top-4 px-6 rounded-full block absolute sm:hidden  bg-verdeBX text-white py-2 drop-shadow-md"> {totalPontos} {totalPontos == 1 ? " ponto " : "pontos" } </div>
-                <div className=" hidden w-28 z-0 absolute sm:block bottom-20 -right-20 bg-verdeBX text-white sm:rounded-full pl-10 spr-2 py-2 drop-shadow-md"> {totalPontos} {totalPontos == 1 ? " ponto " : "pontos" } </div>
-            </section>
+        <section className='pb-[7rem] pt-16 bg-creme'>
             
-            {/* Onda laranja de baixo do circulo que apresenta equipe e membros da equipe */}
-            <div className="relative z-20 mb-8 -mt-16 pt-2 pb-6 sm:pb-4 drop-shadow-md bg-laranjaBX w-full max-w-80 h-fit md:h-52 md:w-80 rounded-tl-[96px] rounded-br-[96px] flex flex-col items-center justify-center">
-                <h2 className={`${lilita.className} text-center px-2 break-words w-48 sm:w-60 my-1 mt-3 sm:my-2 text-md tracking-wide drop-shadow-md text-2xl font-bold`}> {equipes[idSelecionado].nome} </h2>
-                <div className={`${poppins.className} text-center w-full max-w-[18rem] flex flex-col items-center justify-center text-sm text-white`}>
-                    {equipes[idSelecionado].membros.map((membro, idx) => {
-                        return (
-                        <p key={idx} className="truncate w-full px-4 sm:px-0 text-center">{membro}</p>); })}
-                </div>
-            </div>
-        
+            {/* titulo do componente e descrição da seção  */}       
+            <h1 id="display" className="text-azulelegante font-cinzel font-bold text-[3.2rem] pl-20"> eQuipeS </h1>
+            <p className= {`p-4 font-medium text-md text-left text-preto pl-20`}> Conheça os competidores deste ano! </p>
+
+            <section className="flex justify-center [@media(max-width:1100px)]:flex-col">
+
+                {/* parte que mostra os botões de todas as equipes */}
+                <section className="[@media(max-width:1100px)]:w-full w-[50vw] my-4 md:my-2 py-2 md:px-8 lg:px-[15px] flex flex-wrap items-center justify-center">
+                    <section className="gap-8 sm:grid-flow-col flex sm:h-fit flex-wrap items-center justify-center">
+                        { equipes.map((equipe, idx) => {
+                            return <BotaoEquipe 
+                                    key={idx} 
+                                    equipe={equipe} 
+                                    selecionado={idSelecionado==idx} 
+                                    onClick={()=> {setIdSelecionado(idx); scrolltodisplayer();}} />  
+                        })
+                        }
+
+                    </section>
+                </section>
+
+                <section className="overflow-hidden [@media(max-width:1100px)]:mt-[110px] [@media(max-width:1100px)]:w-full w-[50vw] px-4 flex flex-col items-center content-center justify-center">
+                    
+                    <img className="absolute w-[35rem]" src="./ExibeEquipe.svg"></img>
+                    
+                    <div className='relative top-[0.4rem] flex flex-col items-center justify-center'>
+                        <div className="z-0 flex w-[14rem] h-[14rem] justify-center items-center relative border-8 p-[1rem] rounded-full bg-white drop-shadow-lg"> 
+                            <Image className="z-0 w-fit h-fit" alt="" width={130} height={130} src={equipes[idSelecionado].iconPath} />
+                        </div>
+                        <div className="text-center font-bold text-[1rem] pt-[10px]"> {totalPontos} {totalPontos == 1 ? " ponto " : "pontos" } </div>
+
+                        <h2 className={`text-center break-words text-md tracking-wide drop-shadow-md p-2 text-2xl font-bold text-branco`}> {equipes[idSelecionado].nome} </h2>
+                        <div className={`text-center w-full flex flex-col items-center justify-center text-sm text-branco`}>
+                            {equipes[idSelecionado].membros.map((membro, idx) => {
+                                return (
+                                <p key={idx} className="truncate w-full px-4 sm:px-0 text-center">{membro}</p>); })}
+                        </div>
+                    </div>
+                </section>
+
+            </section>
         </section>
-
-    </section>
-
     );
 
 }
